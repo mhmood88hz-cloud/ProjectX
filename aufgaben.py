c71415d7faf42e96763414d4da3f27e3745b289d
Viacheslav Miller <vaceslav_miller@mail.ru>
2026-04-29 13:18:12 +0200

    update

--- aufgaben.py
+++ aufgaben.py
@@ -184,7 +184,8 @@ def aufgabe_029_drehe_liste(werte: list[Any]) -> list[Any]:
 
 # Gruppe: Lexoniarus, Mr-Miller777
 def aufgabe_030_flatten(liste_von_listen: list[list[int]]) -> list[int]:
-    """Führe eine verschachtelte Liste zu einer flachen Liste zusammen."""
+
+
     pass
 
 
