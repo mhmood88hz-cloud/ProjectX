bd9c673dbc60f700d3f0392d8e6f64b4ca23e314
Natasha Libera <tash@artoftash.com>
2026-04-29 12:56:03 +0200

    Implemented Aufgabe 026

--- aufgaben.py
+++ aufgaben.py
@@ -161,7 +161,10 @@ def aufgabe_025_unique_werte(zahlen: list[int]) -> list[int]:
 # Gruppe: Lexoniarus, Mr-Miller777
 def aufgabe_026_finde_index(werte: list[str], wert: str) -> int:
     """Finde den Index von wert, -1 wenn nicht vorhanden."""
-    pass
+    try:
+        return werte.index(wert)
+    except ValueError:
+        return -1
 
 
 # Gruppe: Lexoniarus, Mr-Miller777
